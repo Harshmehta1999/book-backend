3e8709f3930bcbcbb15dd7a657ea32c6e5f44c16
Harshmehta1999 <50991802+Harshmehta1999@users.noreply.github.com>
2020-03-26 14:31:41 +1300

    first

---
+++ connection.js
@@ -0,0 +1,13 @@
+// let dbname = "books_and_things";
+// let username = "kjdone";
+// let password = "root";
+
+let dbname = "bookdata";
+let username = "Harshmehta1999";
+let password = "$$Ronaldo7";
+
+// exports.atlas = `mongodb+srv://${username}:${password}@yoobee2020-vhp1l.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+
+
+exports.atlas = `mongodb+srv://${username}:${password}@cluster0-cjf4h.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`;
